11f4aa606ddb45f389a33bef8bce9bfcc6c1680f
JerryCyHu <2540887746@qq.com>
2021-06-10 11:43:14 -0400

    java1.3

--- TowerDefence/src/backEnd/Coordinate.java
+++ TowerDefence/src/backEnd/Coordinate.java
@@ -1,5 +1,32 @@
 package backEnd;
 
 public class Coordinate {
-
+	private int x, y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	@Override
+	public int hashCode() {
+		int tmp = ( y +  ((x+1)/2));
+        return x +  (tmp * tmp);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj.getClass().equals(this.getClass())) {
+			Coordinate c = (Coordinate) obj;
+			return c.x==this.x&&c.y==this.y;
+		}else {
+			return false;
+		}
+	}
 }
--- TowerDefence/src/backEnd/CoordinatesFacotry.java
+++ TowerDefence/src/backEnd/CoordinatesFacotry.java
@@ -1,7 +1,23 @@
 package backEnd;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CoordinatesFacotry {
-	static public int a(int b) {
-		return b;
+	static HashMap<Coordinate, Coordinate> coordinateMap;
+	static public Coordinate getCoordinate(int x, int y) {
+		Coordinate coordinate = new Coordinate(x, y);
+		if (coordinateMap==null) {
+			coordinateMap=new HashMap<Coordinate, Coordinate>();
+			coordinateMap.put(coordinate, coordinate);
+			return coordinate;
+		}else {
+			if (coordinateMap.containsKey(coordinate)) {
+				return coordinateMap.get(coordinate);
+			}else {
+				coordinateMap.put(coordinate, coordinate);
+				return coordinate;
+			}
+		}
 	}
 }
