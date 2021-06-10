88cfd459c663c567ebfd4408ea8359b1b3fdf76a
JerryCyHu <2540887746@qq.com>
2021-06-10 05:18:01 -0400

    prepare1.2

--- towerDefense/Coordinate.h
+++ towerDefense/Coordinate.h
@@ -5,10 +5,17 @@
 #ifndef TOWERDEFENSE_COORDINATE_H
 #define TOWERDEFENSE_COORDINATE_H
 class Coordinate{
+    int x;
+    int y;
 public:
+    Coordinate(int x, int y){
+        this->x=x;
+        this->y=y;
+    }
     bool isEqual(Coordinate* coordinate){ return true;}
     bool isNeighbor(Coordinate* coordinate){ return true;}
-
+    int  getX(){ return x;};
+    int  gety(){ return y;};
 };
 
 #endif //TOWERDEFENSE_COORDINATE_H
--- towerDefense/CoordinatesFactory.h
+++ towerDefense/CoordinatesFactory.h
@@ -8,9 +8,19 @@
 #include "Coordinate.h"
 #include <unordered_set>
 using namespace std;
+struct myHash
+{
+    size_t  operator()(Coordinate c) const
+    {
+        int x = c.getX();
+        int y = c.gety();
+        int tmp = ( y +  ((x+1)/2));
+        return static_cast<size_t>(x +  ( tmp * tmp));
+    }
+};
 class CoordinatesFactory{
 private:
-    //unordered_set<Coordinate> coordinatesSet;
+    unordered_set<Coordinate, myHash> coordinatesSet;
     CoordinatesFactory(){};
 public:
     //static Coordinate getCoordinates(int x, int y){ return ;}
