d81e842784f2712fc4f3ce15ca88b3a9be60e9b7
JerryCyHu <2540887746@qq.com>
2021-06-10 11:09:32 -0400

    java1.0

--- TowerDefence/src/backEnd/CoordinatesFacotry.java
+++ TowerDefence/src/backEnd/CoordinatesFacotry.java
@@ -1,4 +1,4 @@
-package BackEnd;
+package backEnd;
 
 public class CoordinatesFacotry {
 
---
+++ TowerDefence/test/backEnd/CoordinatesFactoryTest.java
@@ -0,0 +1,14 @@
+package backEnd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoordinatesFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
